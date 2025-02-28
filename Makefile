80fc05aa982cd8fe18a2d784515eec10ba4baf78
sysmenon <menongokuls@gmail.com>
2025-02-28 16:41:40 +0000

    Playing around with Windows' windows. Going to use Visual Studio now

--- Makefile
+++ Makefile
@@ -1,14 +1,23 @@
 
 #LFLAG = C:\Windows\WinSxS\wow64_microsoft-windows-kernel32_31bf3856ad364e35_10.0.26100.3037_none_3a0213b63a909242
-LFLAG = "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.26100.0\um\x64"
-lFLAG = kernel32
+#LFLAG = "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.26100.0\um\x64"
+#lFLAG = kernel32
+
+LFLAG = -L "C:\cygwin64\usr\x86_64-w64-mingw32\sys-root\mingw\bin"  #-llibstdc++-6 
+INCLUDEQT = -I "C:\cygwin64\usr\include\qt5\QtWidgets" -I "C:\cygwin64\usr\include\qt5" -I "C:\cygwin64\usr\include\qt5\QtGui"
 INCLUDE = -I "C:\Program Files (x86)\Windows Kits\10\Include\10.0.26100.0\um" -I "C:\Program Files (x86)\Windows Kits\10\Include\10.0.26100.0\shared" -I "C:\Program Files (x86)\Windows Kits\10\Include\10.0.26100.0" -I "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.43.34808\include" 
 OUT = main.exe
+OUTTEST = test.exe
+OTHERFLAGS = -static
 
+G++ = x86_64-w64-mingw32-g++.exe
 # -L${LFLAG} -l${lFLAG}
 
 default:
-	g++ -Wl,--trace main.cpp -o ${OUT} -v
+	${G++} -Wl,--trace main.cpp -o ${OUT} -v ${LFLAG} ${OTHERFLAGS}
+
+test:
+	${G++} -Wl,--trace test.cpp -o ${OUTTESTtrs} -v ${LFLAG} ${OTHERFLAGS}
 
 clRun:
 	cl.exe ${INCLUDE} main.cpp -o ${OUT} 
@@ -19,5 +28,4 @@ run:
 clean:
 	rm main.exe
 
-
-	"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.43.34808\include"
+#"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.43.34808\include"
--- main.cpp
+++ main.cpp
@@ -5,19 +5,82 @@
 #include <Windows.h>
 #include <iostream> 
 
-
+LRESULT CALLBACK WindowProc( HWND   hWnd,UINT   message, WPARAM wParam,  LPARAM lParam);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow) {
-    
-    
+   
+    const wchar_t CLASS_NAME[] = L"Entry Screen";
+
+    WNDCLASS wc = {};
+
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindowEx(
+        0,                              // Optional window styles.
+        CLASS_NAME,                     // Window class
+        L"System Information",    // Window text
+        WS_OVERLAPPEDWINDOW,            // Window style
+
+        // Size and position
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+        NULL,       // Parent window    
+        NULL,       // Menu
+        hInstance,  // Instance handle
+        NULL        // Additional application data
+    );
     _SYSTEM_INFO systemInfo;
 
     GetNativeSystemInfo(&systemInfo);
 
+    if (hwnd == NULL)
+    {
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+
+    // Run the message loop.
+
+    MSG msg = { };
+    while (GetMessage(&msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
     
     std::cout << systemInfo.wProcessorLevel << std::endl;
     std::cout << systemInfo.dwNumberOfProcessors << std::endl;
-    char x;
-    std::cin >> x;
+    
     return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+
+            // All painting occurs here, between BeginPaint and EndPaint.
+
+            FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
+
+            EndPaint(hwnd, &ps);
+        }
+        return 0;
+
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
